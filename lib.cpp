693a7a0d743dc660a4121e2fc1bd2d7f951d097d
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2022-12-17 01:42:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
+bool cerca(char nomi[10][20], int &posizione, char check[]){
+    for (int i = 0; i <10 ; ++i) {
+        for (int j = 0; j < 20; ++j) {
+            if (nomi[i][j] != check[j]) {
+                j = 20;
+            } else {
+                if (j == 19) {
+                    posizione = i;
+                    return true;
+                }
+            }
 
+        }
+    }
+    return false;
+}
