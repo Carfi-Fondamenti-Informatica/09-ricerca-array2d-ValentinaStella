c308dabe7ecaf1239192463db8ec27e216d83165
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2022-12-17 01:40:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char nomi[10][20];
+    char check[20];
+    int pos;
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 20; ++j) {
+            check[j]=' ';
+            nomi[i][j]=' ';
+
+
+        }
+
+    }
+    for (int i = 0; i < 10; ++i) {
+        cin>>nomi[i];
+
+    }
+    cin>>check;
+    if(cerca(nomi,pos, check)){
+        cout<<pos;
+    }else{
+        cout<<"non presente";
+    }
+    return 0;
 }
